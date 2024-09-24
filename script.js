74a3d076b2211e25035b9a6d8e4cd2ed5cef9cc3
y2kath <jramirezm2211@alumno.ipn.mx>
2024-09-24 17:56:34 -0600

    creacion de directorio y archivo

---
+++ script.js
@@ -0,0 +1,26 @@
+//CLASE 24/09/2024
+//NOMBRE: RAMIREZ MEDINA JEANNETTE CATHERINE
+
+//variables
+/* 
+las variables guardan informacion que puedes reutilizar
+*/
+//declaracion de variables
+
+let nombre = 'Pedro'; //usa let para declarar varables qur pueden cambiar su valor, va en bloques de codigo
+const PI = 3.1416; 
+var edad = 25; //se puede ponber en cualquier linea de codigo, no importa el orden
+
+//operadores
+let a = 5;
+let b = 2;
+let suma = a + b; //7
+let resta = a - b; //3
+let multiplicacion = a*b; //10
+let division = a/b; //2.5
+
+//operadores de comparación
+a > b; //true
+a < b; //false
+a == b; //false
+a === b; //false (compara valor y tipo)
