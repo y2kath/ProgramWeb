d7b9d2095aff0f65fa78145cbca10fd1b524feb0
y2kath <160188575+y2kath@users.noreply.github.com>
2024-10-24 16:37:57 -0600

    CLASE24102024

---
+++ CLASE24102024/script.js
@@ -0,0 +1,50 @@
+//arreglos
+/*
+let array = ["dato1",1,Boolean];
+console.log("tamaño array: "+array.length);
+array.push("insertar nuevo val");
+console.log(array[3]);
+console.log("nuevo tamaño array: "+array.length);
+console.log(array[3]);
+*/
+//hacer tabla del 5 que se detenga cuando la memoria llegue a los 6 espacios
+/*
+5*1 = 5
+5*2 = 10
+5*3 = 15
+5*4 = 20
+5*5 = 25
+5*6 = 30
+fin de la posicion = 30
+
+
+let multiplicacion = [];
+ for (let a = 1; a < 10; a++) {
+    r = 5*(a*1);
+    if (r<=30) {
+        multiplicacion.push(r);
+    }
+ }
+
+for (let b = 0; b < multiplicacion.length; b++) {
+    console.log(multiplicacion[b]);
+}
+
+console.log("Fin de la posicion en 30");
+*/
+
+//ELIMINAR ELEMENTOS DE UNA ARRAY
+let array = ["dato1","dato2","dato3"];
+array.push("dato4");
+console.log(array[3]);
+
+//ELIMINAR
+array.splice(1,1);
+console.log(array);
+
+//Hacer una lista de tareas
+
+
+
+
+
